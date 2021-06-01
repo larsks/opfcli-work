larsks/opfcli

Add tests for api module
This is a separate commit so that it can be filtered out while
reviewing the code changes from the adjacent commits.

api/AddGroupRBAC_test.go
@@ -1,0 +1,69 @@
+package api
+
+import (
+	"path/filepath"
+
+	"github.com/stretchr/testify/require"
+)
+
+func (ctx *Context) TestAddGroupRBAC() {
+	assert := require.New(ctx.T())
+
+	// Should fail if project does not exist
+	err := ctx.api.AddGroupRBAC(
+		"testproject",
+		"testgroup2",
+		"admin",
+	)
+	assert.EqualError(err, "namespace testproject does not exist")
+
+	// ---
+
+	// Should fail if group does not exist
+	err = ctx.api.CreateProject(
+		"testproject",
+		"testgroup",
+		"test description",
+	)
+	assert.Nil(err)
+
+	err = ctx.api.AddGroupRBAC(
+		"testproject",
+		"testgroup2",
+		"admin",
+	)
+	assert.EqualError(err, "group testgroup2 does not exist")
+
+	// ---
+
+	// Should work if both project and group exist
+	err = ctx.api.CreateGroup(
+		"testgroup2",
+		false,
+	)
+	assert.Nil(err)
+	assert.FileExists(filepath.Join(
+		ctx.dir,
+		"cluster-scope/base/user.openshift.io/groups/testgroup2/group.yaml",
+	))
+
+	err = ctx.api.AddGroupRBAC(
+		"testproject",
+		"testgroup2",
+		"admin",
+	)
+	assert.Nil(err)
+
+	expectedPaths := []string{
+		"cluster-scope/base/core/namespaces/testproject/namespace.yaml",
+		"cluster-scope/base/core/namespaces/testproject/kustomization.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup/group.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup/kustomization.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup2/group.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup2/kustomization.yaml",
+		"cluster-scope/components/project-admin-rolebindings/testgroup/rbac.yaml",
+		"cluster-scope/components/project-admin-rolebindings/testgroup2/rbac.yaml",
+	}
+
+	compareWithExpected(assert, "testdata/AddGroupRBAC", ctx.dir, expectedPaths)
+}

api/CreateGroup_test.go
@@ -1,0 +1,38 @@
+package api
+
+import "github.com/stretchr/testify/require"
+
+func (ctx *Context) TestCreateGroup() {
+	assert := require.New(ctx.T())
+
+	err := ctx.api.CreateGroup(
+		"testgroup",
+		false,
+	)
+	assert.Nil(err)
+
+	expectedPaths := []string{
+		"cluster-scope/base/user.openshift.io/groups/testgroup/group.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup/kustomization.yaml",
+	}
+
+	compareWithExpected(assert, "testdata/CreateGroup", ctx.dir, expectedPaths)
+
+	// ---
+
+	// Should fail if group already exists and existsOk is false
+	err = ctx.api.CreateGroup(
+		"testgroup",
+		false,
+	)
+	assert.EqualError(err, "group testgroup already exists")
+
+	// ---
+
+	// Should work if group already exists and existsOk is true
+	err = ctx.api.CreateGroup(
+		"testgroup",
+		true,
+	)
+	assert.Nil(err)
+}

api/CreateNamespace_test.go
@@ -1,0 +1,21 @@
+package api
+
+import "github.com/stretchr/testify/require"
+
+func (ctx *Context) TestCreateNamespace() {
+	assert := require.New(ctx.T())
+
+	err := ctx.api.CreateNamespace(
+		"testproject",
+		"testgroup",
+		"test description",
+	)
+	assert.Nil(err)
+
+	expectedPaths := []string{
+		"cluster-scope/base/core/namespaces/testproject/kustomization.yaml",
+		"cluster-scope/base/core/namespaces/testproject/namespace.yaml",
+	}
+
+	compareWithExpected(assert, "testdata/CreateNamespace", ctx.dir, expectedPaths)
+}

api/CreateProject_test.go
@@ -1,0 +1,24 @@
+package api
+
+import "github.com/stretchr/testify/require"
+
+func (ctx *Context) TestCreateProject() {
+	assert := require.New(ctx.T())
+
+	err := ctx.api.CreateProject(
+		"testproject",
+		"testgroup",
+		"test description",
+	)
+	assert.Nil(err)
+
+	expectedPaths := []string{
+		"cluster-scope/base/core/namespaces/testproject/namespace.yaml",
+		"cluster-scope/base/core/namespaces/testproject/kustomization.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup/group.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup/kustomization.yaml",
+		"cluster-scope/components/project-admin-rolebindings/testgroup/rbac.yaml",
+	}
+
+	compareWithExpected(assert, "testdata/CreateProject", ctx.dir, expectedPaths)
+}

api/CreateRoleBinding_test.go
@@ -1,0 +1,20 @@
+package api
+
+import "github.com/stretchr/testify/require"
+
+func (ctx *Context) TestCreateRoleBinding() {
+	assert := require.New(ctx.T())
+
+	err := ctx.api.CreateRoleBinding(
+		"testgroup",
+		"admin",
+	)
+	assert.Nil(err)
+
+	expectedPaths := []string{
+		"cluster-scope/components/project-admin-rolebindings/testgroup/rbac.yaml",
+		"cluster-scope/components/project-admin-rolebindings/testgroup/kustomization.yaml",
+	}
+
+	compareWithExpected(assert, "testdata/CreateRoleBinding", ctx.dir, expectedPaths)
+}

api/main_test.go
@@ -1,0 +1,42 @@
+package api
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type Context struct {
+	suite.Suite
+	dir string
+	api *API
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(Context))
+}
+
+func (ctx *Context) SetupTest() {
+	ctx.dir = ctx.T().TempDir()
+	ctx.api = New("cluster-scope", ctx.dir)
+}
+
+func compareWithExpected(assert *require.Assertions, expectedRoot, actualRoot string, paths []string) {
+	for _, path := range paths {
+		expectedPath := filepath.Join(expectedRoot, path)
+		actualPath := filepath.Join(actualRoot, path)
+
+		assert.FileExists(actualPath)
+
+		actualData, err := ioutil.ReadFile(actualPath)
+		assert.Nil(err)
+
+		expectedData, err := ioutil.ReadFile(expectedPath)
+		assert.Nil(err)
+
+		assert.YAMLEq(string(expectedData), string(actualData))
+	}
+}

larsks/opfcli-work

api/AddGroupRBAC_test.go
@@ -1,0 +1,79 @@
+package api
+
+import (
+	"path/filepath"
+
+	"github.com/stretchr/testify/require"
+)
+
+func (suite *apiTestSuite) TestAddGroupRBAC() {
+	assert := require.New(suite.T())
+
+	// Should fail if role does not exist
+	err := suite.api.AddGroupRBAC(
+		"testproject",
+		"testgroup2",
+		"testrole",
+	)
+	assert.EqualError(err, "role testrole does not exist")
+
+	// ---
+
+	// Should fail if project does not exist
+	err = suite.api.AddGroupRBAC(
+		"testproject",
+		"testgroup2",
+		"admin",
+	)
+	assert.EqualError(err, "namespace testproject does not exist")
+
+	// ---
+
+	// Should fail if group does not exist
+	err = suite.api.CreateProject(
+		"testproject",
+		"testgroup",
+		"test description",
+	)
+	assert.Nil(err)
+
+	err = suite.api.AddGroupRBAC(
+		"testproject",
+		"testgroup2",
+		"admin",
+	)
+	assert.EqualError(err, "group testgroup2 does not exist")
+
+	// ---
+
+	// Should work if both project and group exist
+	err = suite.api.CreateGroup(
+		"testgroup2",
+		false,
+	)
+	assert.Nil(err)
+	assert.FileExists(filepath.Join(
+		suite.dir,
+		"cluster-scope/base/user.openshift.io/groups/testgroup2/group.yaml",
+	))
+
+	err = suite.api.AddGroupRBAC(
+		"testproject",
+		"testgroup2",
+		"admin",
+	)
+	assert.Nil(err)
+
+	expectedPaths := []string{
+		"cluster-scope/base/core/namespaces/testproject/namespace.yaml",
+		"cluster-scope/base/core/namespaces/testproject/kustomization.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup/group.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup/kustomization.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup2/group.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup2/kustomization.yaml",
+		"cluster-scope/components/project-admin-rolebindings/testgroup/rbac.yaml",
+		"cluster-scope/components/project-admin-rolebindings/testgroup2/rbac.yaml",
+	}
+
+	compareWithExpected(assert, "testdata/AddGroupRBAC", suite.dir, expectedPaths)
+}

api/CreateGroup_test.go
@@ -1,0 +1,38 @@
+package api
+
+import "github.com/stretchr/testify/require"
+
+func (suite *apiTestSuite) TestCreateGroup() {
+	assert := require.New(suite.T())
+
+	err := suite.api.CreateGroup(
+		"testgroup",
+		false,
+	)
+	assert.Nil(err)
+
+	expectedPaths := []string{
+		"cluster-scope/base/user.openshift.io/groups/testgroup/group.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup/kustomization.yaml",
+	}
+
+	compareWithExpected(assert, "testdata/CreateGroup", suite.dir, expectedPaths)
+
+	// ---
+
+	// Should fail if group already exists and existsOk is false
+	err = suite.api.CreateGroup(
+		"testgroup",
+		false,
+	)
+	assert.EqualError(err, "group testgroup already exists")
+
+	// ---
+
+	// Should work if group already exists and existsOk is true
+	err = suite.api.CreateGroup(
+		"testgroup",
+		true,
+	)
+	assert.Nil(err)
+}

api/CreateNamespace_test.go
@@ -1,0 +1,21 @@
+package api
+
+import "github.com/stretchr/testify/require"
+
+func (suite *apiTestSuite) TestCreateNamespace() {
+	assert := require.New(suite.T())
+
+	err := suite.api.CreateNamespace(
+		"testproject",
+		"testgroup",
+		"test description",
+	)
+	assert.Nil(err)
+
+	expectedPaths := []string{
+		"cluster-scope/base/core/namespaces/testproject/kustomization.yaml",
+		"cluster-scope/base/core/namespaces/testproject/namespace.yaml",
+	}
+
+	compareWithExpected(assert, "testdata/CreateNamespace", suite.dir, expectedPaths)
+}

api/CreateProject_test.go
@@ -1,0 +1,24 @@
+package api
+
+import "github.com/stretchr/testify/require"
+
+func (suite *apiTestSuite) TestCreateProject() {
+	assert := require.New(suite.T())
+
+	err := suite.api.CreateProject(
+		"testproject",
+		"testgroup",
+		"test description",
+	)
+	assert.Nil(err)
+
+	expectedPaths := []string{
+		"cluster-scope/base/core/namespaces/testproject/namespace.yaml",
+		"cluster-scope/base/core/namespaces/testproject/kustomization.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup/group.yaml",
+		"cluster-scope/base/user.openshift.io/groups/testgroup/kustomization.yaml",
+		"cluster-scope/components/project-admin-rolebindings/testgroup/rbac.yaml",
+	}
+
+	compareWithExpected(assert, "testdata/CreateProject", suite.dir, expectedPaths)
+}

api/CreateRoleBinding_test.go
@@ -1,0 +1,20 @@
+package api
+
+import "github.com/stretchr/testify/require"
+
+func (suite *apiTestSuite) TestCreateRoleBinding() {
+	assert := require.New(suite.T())
+
+	err := suite.api.CreateRoleBinding(
+		"testgroup",
+		"admin",
+	)
+	assert.Nil(err)
+
+	expectedPaths := []string{
+		"cluster-scope/components/project-admin-rolebindings/testgroup/rbac.yaml",
+		"cluster-scope/components/project-admin-rolebindings/testgroup/kustomization.yaml",
+	}
+
+	compareWithExpected(assert, "testdata/CreateRoleBinding", suite.dir, expectedPaths)
+}

api/main_test.go
@@ -1,0 +1,44 @@
+package api
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/operate-first/opfcli/utils"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type apiTestSuite struct {
+	suite.Suite
+	dir string
+	api *API
+}
+
+func TestAPI(t *testing.T) {
+	utils.ConfigureLogging()
+	suite.Run(t, new(apiTestSuite))
+}
+
+func (suite *apiTestSuite) SetupTest() {
+	suite.dir = suite.T().TempDir()
+	suite.api = New("cluster-scope", suite.dir)
+}
+
+func compareWithExpected(assert *require.Assertions, expectedRoot, actualRoot string, paths []string) {
+	for _, path := range paths {
+		expectedPath := filepath.Join(expectedRoot, path)
+		actualPath := filepath.Join(actualRoot, path)
+
+		assert.FileExists(actualPath)
+
+		actualData, err := ioutil.ReadFile(actualPath)
+		assert.Nil(err)
+
+		expectedData, err := ioutil.ReadFile(expectedPath)
+		assert.Nil(err)
+
+		assert.YAMLEq(string(expectedData), string(actualData))
+	}
+}